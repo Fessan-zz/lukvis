e52abe58a1c1102485d6078ffd82ea48522125e3
Fessan <fessan@ya.ru>
2020-06-20 12:14:16 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './assets/styles/custom.scss';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
